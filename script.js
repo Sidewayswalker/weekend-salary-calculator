185acd246fe0a05149f986e2eb1560af0775a870
Steven Karl <Karl.Steven09@gmail.com>
2024-04-01 20:29:59 -0500

    fixed formatting of over-budget. and added formatAsUSD

--- script.js
+++ script.js
@@ -3,59 +3,56 @@ let totalMonthlyCost = 0;
 
 //Create a function that adds employee data to a table. 
 function createEmployee(event) {
-    // Prevent default HTML form.
-    event.preventDefault();
+  // Prevent default HTML form.
+  event.preventDefault();
 
-    // Establish T-variables in JS that identify HTML input values.
-    let firstNameInput = document.getElementById("first name").value;
-    let lastNameInput = document.getElementById("last name").value;
-    let idNumberInput = document.getElementById("id number").value;
-    let jobTitleInput = document.getElementById("job title").value;
-    let annualSalaryInput = document.getElementById("annual salary").value;
+  // Establish T-variables in JS that identify HTML input values.
+  let firstNameInput = document.getElementById("first name").value;
+  let lastNameInput = document.getElementById("last name").value;
+  let idNumberInput = document.getElementById("id number").value;
+  let jobTitleInput = document.getElementById("job title").value;
+  let annualSalaryInput = document.getElementById("annual salary").value;
 
-    // Establish T-variable that determines the format of the input values.
-    // last row in the table '❌' is a button now. 
-    // "on click" call "DeleteRow" function.
-    let newTableRow = `
-    <tr>
-      <td>${firstNameInput}</td>
-      <td>${lastNameInput}</td>
-      <td>${idNumberInput}</td>
-      <td>${jobTitleInput}</td>
-      <td>${annualSalaryInput}</td>
-      <td><button onclick="deleteRow(event)">❌</button></td>
-    </tr>
+  // Establish T-variable that determines the format of the input values.
+  // last row in the table '❌' is a button now. 
+  // "on click" call "DeleteRow" function.
+  let newTableRow = `
+  <tr>
+    <td>${firstNameInput}</td>
+    <td>${lastNameInput}</td>
+    <td>${idNumberInput}</td>
+    <td>${jobTitleInput}</td>
+    <td>${annualSalaryInput}</td>
+    <td><button onclick="deleteRow(event)">❌</button></td>
+  </tr>
     `
 
-    // Established T-variable that references the location of the table body in HTML. 
-    // Add a new row to the table body. 
-    let tableLocation = document.getElementById("employee-Table");
-    tableLocation.innerHTML += newTableRow
+  // Established T-variable that references the location of the table body in HTML. 
+  // Add a new row to the table body. 
+  let tableLocation = document.getElementById("employee-Table");
+  tableLocation.innerHTML += newTableRow
 
-    // Reset the values of the "input" boxes to an empyy string.
-    document.getElementById('first name').value = '';
-    document.getElementById('last name').value = '';
-    document.getElementById('id number').value = '';
-    document.getElementById('job title').value = '';
-    document.getElementById('annual salary').value = '';
+  // Reset the values of the "input" boxes to an empyy string.
+  document.getElementById('first name').value = '';
+  document.getElementById('last name').value = '';
+  document.getElementById('id number').value = '';
+  document.getElementById('job title').value = '';
+  document.getElementById('annual salary').value = '';
 
-    // G-variable "totalMonthlyCost" += annualSalaryInput
-    totalMonthlyCost += Math.round(Number(annualSalaryInput)/12);
+  // G-variable "totalMonthlyCost" += annualSalaryInput
+  totalMonthlyCost += Math.round(Number(annualSalaryInput)/12);
 
-    // T-variable that references HTML location of the "totalMonthlyCost" 
-    // The location in the HTML is being given the "totalMonthlyCost" variable. 
-    let salarySum = document.getElementById('salary-Count')
-    salarySum.textContent = totalMonthlyCost;
+  // T-variable that references HTML location of the "totalMonthlyCost" 
+  // The location in the HTML is being given the "totalMonthlyCost" variable. 
+  let salarySum = document.getElementById('salary-Count')
+  salarySum.textContent = totalMonthlyCost;
 
-    // Create an if statement.
-    // T-variable referencing the footer class.
-    // looping though all the classes within HTML [<p>] and changing all the backgrounds in the footer to red.
-    if(totalMonthlyCost > 20000){
-      let theFooter = document.getElementsByClassName("over-budget");
-      for (let i=0; i<theFooter.length; i++){
-        theFooter[i].style.background = "red";
-      }
-    }
+// assign CSS formatting to 'over-budget'
+// if it goes over 20000 give the footer a class of over-budget
+  if(totalMonthlyCost > 20000){
+    let footer = document.getElementById('myFooter')
+    footer.classList.add('over-budget')
+  }
 }
 
 // Create a function to delete the target of a button click.
@@ -63,7 +60,14 @@ function createEmployee(event) {
 function deleteRow(event) {
     let buttonclicked = event.target;
     let toDelete = buttonclicked.parentElement.parentElement;
+    // let toDelete =  buttonclicked.closest('tr')
     toDelete.remove();
 
   }
   
+  function formatAsUSD(number) {
+    return new Intl.NumbersFormat(
+      'en-EN',
+      { style: 'currency', currency: 'USD' }
+    ).format(number);
+  }
